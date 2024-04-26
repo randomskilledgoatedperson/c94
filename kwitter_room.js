f261b726b47118c8c24ade2694208ebd083a02fb
randomskilledgoatedperson <166881950+randomskilledgoatedperson@users.noreply.github.com>
2024-04-26 17:29:02 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD42-qi0NomkG3xGVcjpWWQwkGJhWGiyJI",
+    authDomain: "kwitterproject1.firebaseapp.com",
+    projectId: "kwitterproject1",
+    storageBucket: "kwitterproject1.appspot.com",
+    messagingSenderId: "124567440086",
+    appId: "1:124567440086:web:0fcb4f93a49f981068d777",
+    measurementId: "G-GRRJNG8F43"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  
